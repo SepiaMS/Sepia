b1d33b1ca6d582c7d7323b8f838e13687e1413cd
Diti Torterat <kra@diti.me>
2016-05-04 18:31:20 +0200

    Actual scaling

---
+++ src/app_unity/Assets/QuestMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadQuestByName(string quest)
+	{
+		Debug.Log ("Loading level “" + quest + "”");
+		Application.LoadLevel (quest);
+	}
+}
