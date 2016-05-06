0e5d6be19a773f764af9457789d1999b6eafc486
varin-pierre <pvarin@student.42.fr>
2016-05-06 18:52:40 +0200

    fix selection menu quest

---
+++ src/app_unity/Assets/Click2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Click2 : MonoBehaviour, IPointerClickHandler {
+	public Click[] clicks;
+
+
+	public void OnPointerClick (PointerEventData eventData) {
+		Debug.Log ("Hello");
+		foreach (Click c in clicks) {
+			if (c.Truc()) {
+				Debug.Log("Good" + c.name);
+				return ;
+			}
+			else {
+//				Application.LoadLevel(c.level);
+			}
+		}
+	}
+}
---
+++ src/app_unity/Assets/Scripts/GameControler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GameControler : MonoBehaviour {
+
+//	public float	timer;
+
+//	private Text	timerText;
+	private Text	scoreText;
+//	private Text	comboText;
+	private int		score;
+//	private int		combo;
+
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+//		combo = 0;
+//		if (timer == null)
+//			timer = 60.0f;
+	}
+
+	// Update is called once per frame
+	void Update (){
+		score += 1;
+		scoreText.text = "Score: " + score.ToString();
+//		timer -= Time.deltaTime;
+	}
+	void UpdateScore () {
+		
+	}
+
+
+}
