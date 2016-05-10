e3303acf601421ac26965bedac1d626f61ace8f6
varin-pierre <pvarin@student.42.fr>
2016-05-10 23:20:49 +0200

    fix quest dex image + add int scene

---
+++ src/app_unity/Assets/Images/Int/GameControllerInt.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GameControllerInt : MonoBehaviour {
+
+	public float	timer;
+	public Text		timerText;
+	private float	cd; 			//for "sablier" set with timer
+	public Image	cooldown;
+
+	public GameObject test;
+	public Sprite spriteSuccess;
+	// Use this for initialization
+	void Start () {
+		cd = timer;
+		printText ();
+		SequenceDemo();
+	}
+
+	// Update is called once per frame
+	void Update (){
+		
+//		test.GetComponent<SpriteRenderer>().sprite = spriteSuccess;
+		
+		if (timer > 0) {	
+			cooldown.fillAmount -= 1.0f/cd * Time.deltaTime; //fill sablier
+			timer -= Time.deltaTime;
+		} 
+		printText ();
+		test = GameObject.Find("1");
+	}
+
+
+	void printText () {
+		if (timer <= 0)
+			timer = 0;
+		timerText.text = timer.ToString ("0.00");		// "0:00:00" format is possible 
+	}
+
+	public void SequenceDemo() {
+		int[] seq = new int[5];
+		for (int i =0; i < 5; i++)
+			seq [i] = Random.Range(1, 7);
+		foreach ( int e in seq )
+		{
+
+			showSequence( (int)e );
+
+		}
+	}
+
+	private int showSequence(int e){
+
+		test = GameObject.Find(e.ToString());
+		test.GetComponent<Image> ().sprite = spriteSuccess;
+		print ("e = " + e.ToString());
+		return 0;
+	}
+}
